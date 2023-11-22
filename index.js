cc427d4d69a5e74d89df5d7582a8ed799567d2c7
tgoldab <117780290+tgoldab@users.noreply.github.com>
2023-11-22 06:36:03 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+const { ApolloServer } = require("apollo-server"); // Import Apollo Server
+const { importSchema } = require("graphql-import"); // Import graphql-import to load schema
+const EtherDataSource = require("./datasource/ethDatasource"); // Import custom EtherDataSource 
+
+require("dotenv").config(); // Load environment variables from .env file
+
+const resolvers = {
+  Query: {
+    etherBalanceByAddress: (root, _args, { dataSources }) => // Resolver to get ether balance for an address
+      dataSources.ethDataSource.etherBalanceByAddress(),
+
+    totalSupplyOfEther: (root, _args, { dataSources }) => // Resolver to get total ether supply
+      dataSources.ethDataSource.totalSupplyOfEther(),
+
+    latestEthereumPrice: (root, _args, { dataSources }) => // Resolver to get latest ETH price
+      dataSources.ethDataSource.getLatestEthereumPrice(),
+
+    blockConfirmationTime: (root, _args, { dataSources }) => // Resolver to get average block confirmation time
+      dataSources.ethDataSource.getBlockConfirmationTime(),
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs, 
+  resolvers,
+  dataSources: () => ({
+    ethDataSource: new EtherDataSource(), // Instantiate EtherDataSource
+  }), 
+});
+
+server.timeout = 0;
+server.listen("9000").then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`); 
+});
+
